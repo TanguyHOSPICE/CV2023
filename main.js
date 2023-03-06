fc4cd69650545204209e26327da4160a385ba511
Ht <htcvhs971@gmail.com>
2023-03-06 13:20:18 +0100

    SKILLS: Add css color file Add css style Init variable For js

---
+++ main.js
@@ -0,0 +1,25 @@
+import {} from './css/';
+// Icons
+const htmlLogo = document.querySelector('.fa-html5');
+const cssLogo = document.querySelector('.fa-css3');
+const jsLogo = document.querySelector('.fa-js');
+const phpLogo = document.querySelector('.fa-php');
+const symfonyLogo = document.querySelector('.fa-symfony');
+const mongoLogo = document.querySelector('img[alt="mongoDB logo"]');
+const mySqlLogo = document.querySelector('img[alt="mysql logo"]');
+const bootstrapLogo = document.querySelector('.fa-bootstrap');
+const reactLogo = document.querySelector('.fa-react');
+const wordpressLogo = document.querySelector('.fa-wordpress');
+const nodeLogo = document.querySelector('.fa-node');
+const sassLogo = document.querySelector('.fa-sass');
+
+const expressLogo = document.querySelector('img[alt="express logo"]');
+const tailwindLogo = document.querySelector('img[alt="tailwind logo"]');
+const jQueryLogo = document.querySelector('img[alt="jquery logo"]');
+const photoshopLogo = document.querySelector('img[alt="photoshop logo"]');
+const illustratorLogo = document.querySelector('img[alt="illustrator logo"]');
+const microsoftOfficeLogo = document.querySelector('img[alt="microsoft office logo"]');
+const autocadLogo = document.querySelector('img[alt="autocad logo"]');
+const openOfficeLogo = document.querySelector('img[alt="open office logo"]');
+
+htmlLogo.style.getPropertyValue('--html-txt-color');
